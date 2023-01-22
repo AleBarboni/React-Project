a74069f4aadcaf8e9f7bc27a19abe26f8e5ee447
AleBarboni <alexa_wera@yahoo.com>
2023-01-22 10:23:18 +0200

    added data

---
+++ pharmacy-stock-register/src/assets/Components/data.js
@@ -0,0 +1,32 @@
+const data = [
+    { 
+        numeProdus: "NUROFEN EXPRESS FORTE 400 mg CAPS. MOI",
+        otc: "DA",
+        stoc: 52,
+        pret: 32.45,
+        dataExpirarii: "2027-05-31"
+    },
+    { 
+        numeProdus: "VIBROCIL 2,5 mg/0,25mg/ml PIC. NAZALE, SOL.",
+        otc: "DA",
+        stoc: 21,
+        pret: 27.67,
+        dataExpirarii: "2025-07-31"
+    },
+    { 
+        numeProdus: "AUGMENTIN 875 mg/125 mg COMPR. FILM.",
+        otc: "NU",
+        stoc: 12,
+        pret: 18.92,
+        dataExpirarii: "2024-11-30"
+    },
+    { 
+        numeProdus: "KETONAL DUO 150 mg CAPS. CU ELIB. PREL.",
+        otc: "NU",
+        stoc: 17,
+        pret: 13.59,
+        dataExpirarii: "2025-03-31"
+    }
+]
+
+export default data;
